a6165c4468dc7e326e9e439a99ba094010e890ad
Max <sulimenko.maxim@gmail.com>
2025-10-18 10:21:51 +0700

    add config loading

---
+++ internal/articles/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+)
+
+const (
+	standardConfigPath = "./configs/articles.yaml"
+)
+
+type Config struct {
+	Env string `yaml:"env" env:"ENV" env-default:"local"`
+}
+
+func Load() *Config {
+
+	configPath := os.Getenv("ARTICLES_CONFIG_PATH")
+	if configPath == "" {
+		configPath = standardConfigPath
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("has no articles config file, %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("error reading config file, %s", configPath)
+	}
+
+	return &cfg
+}
