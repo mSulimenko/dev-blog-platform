64da34adcdc9292e3c14623907c9edf9cf398724
Max <sulimenko.maxim@gmail.com>
2025-10-22 16:04:03 +0700

    add users service

---
+++ internal/articles/dto/user.go
@@ -0,0 +1,16 @@
+package dto
+
+import "time"
+
+type UserReq struct {
+	Username string `json:"username" validate:"required,min=3,max=30"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=4,max=50"`
+}
+
+type UserResp struct {
+	ID        string    `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
