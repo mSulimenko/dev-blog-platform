2bbaf6cd0b74c9ecab0edaeabf0cc76ac9e0874a
Max <sulimenko.maxim@gmail.com>
2025-10-30 21:48:15 +0700

    delete redundant method

--- internal/shared/events/dto/user_events.go
+++ internal/shared/events/dto/user_events.go
@@ -1,13 +1,7 @@
 package dto
 
-import "encoding/json"
-
 type UserRegisteredEvent struct {
 	Email    string `json:"email"`
 	Token    string `json:"token"`
 	Username string `json:"username"`
-}
-
-func (e *UserRegisteredEvent) ToJson() ([]byte, error) {
-	return json.Marshal(e)
 }
